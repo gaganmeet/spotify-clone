11d71cab915b9f30c0a6e7f709e991aedcfd6b67
Gaganmeet Bahri <gaganmeet.bahri96@gmail.com>
2022-09-11 11:30:35 +0530

    feat: added playerlayout

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,3 +1,5 @@
+import NextImage from 'next/image'
+import Link from 'next/link'
 import {
   Box,
   List,
@@ -16,6 +18,39 @@ import {
   MdFavorite,
 } from 'react-icons/md'
 
+const navMenu = [
+  {
+    name: 'Home',
+    icon: MdHome,
+    route: '/',
+  },
+  {
+    name: 'Search',
+    icon: MdSearch,
+    route: '/search',
+  },
+  {
+    name: 'Your Library',
+    icon: MdLibraryMusic,
+    route: '/library',
+  },
+]
+
+const musicMenu = [
+  {
+    name: 'Create Playlist',
+    icon: MdPlaylistAdd,
+    route: '/',
+  },
+  {
+    name: 'Liked Songs',
+    icon: MdFavorite,
+    route: '/',
+  },
+]
+
+const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i}`)
+
 function Sidebar() {
   return (
     <Box
@@ -27,7 +62,87 @@ function Sidebar() {
         height: 'calc(100vh - 100px)',
       }}
     >
-      Sidebar
+      <Box paddingY="20px" height="100%">
+        <Box width="120px" marginBottom="20px" paddingX="20px">
+          <NextImage src="/logo.svg" height={60} width={120} />
+        </Box>
+        <Box marginBottom="30px">
+          <List spacing={3}>
+            {navMenu.map((item) => (
+              <LinkBox key={item.name}>
+                <ListItem
+                  display="flex"
+                  alignItems="center"
+                  paddingX="20px"
+                  paddingY="5px"
+                  borderRadius="5px"
+                  _hover={{
+                    bg: 'gray.700',
+                  }}
+                >
+                  <Link href={item.route} passHref>
+                    <>
+                      <ListIcon as={item.icon} />
+                      <LinkOverlay href={item.route}>{item.name}</LinkOverlay>
+                    </>
+                  </Link>
+                </ListItem>
+              </LinkBox>
+            ))}
+          </List>
+        </Box>
+        <Box marginBottom="10px">
+          <List spacing={3}>
+            {musicMenu.map((item) => (
+              <LinkBox key={item.name}>
+                <ListItem
+                  display="flex"
+                  alignItems="center"
+                  paddingX="20px"
+                  paddingY="5px"
+                  borderRadius="5px"
+                  _hover={{
+                    bg: 'gray.700',
+                  }}
+                >
+                  <Link href={item.route} passHref>
+                    <>
+                      <ListIcon as={item.icon} />
+                      <LinkOverlay href={item.route}>{item.name}</LinkOverlay>
+                    </>
+                  </Link>
+                </ListItem>
+              </LinkBox>
+            ))}
+          </List>
+        </Box>
+        <Divider />
+        <Box overflowY="auto" height="73%" paddingY="20px">
+          <List spacing={3}>
+            {playlists.map((item) => (
+              <LinkBox key={item}>
+                <ListItem
+                  display="flex"
+                  alignItems="center"
+                  paddingX="20px"
+                  paddingY="5px"
+                  borderRadius="5px"
+                  _hover={{
+                    bg: 'gray.700',
+                  }}
+                >
+                  <Link href="/" passHref>
+                    <>
+                      <ListIcon as={MdFavorite} />
+                      <LinkOverlay href="/">{item}</LinkOverlay>
+                    </>
+                  </Link>
+                </ListItem>
+              </LinkBox>
+            ))}
+          </List>
+        </Box>
+      </Box>
     </Box>
   )
 }
